19920375f0d50beeca4fe3494d0d21b669feb69b
moldcraft <moldcraft@email.com>
2014-11-26 18:57:59 +0200

    Added the update parameter

--- manifest.php
+++ manifest.php
@@ -5,7 +5,9 @@
 $manifest = array();
 
 $manifest['name']        = __( 'Portfolio', 'fw' );
-$manifest['description'] = __( "Adds a sliders module to your website from where you'll be able to create different built in jQuery sliders for your homepage and rest of the pages.", 'fw' );
+$manifest['description'] = __( "This extension will add a fully fledged portfolio module that will let you display your projects using the built in portfolio pages.", 'fw' );
 $manifest['version'] = '1.0.0';
 $manifest['display'] = true;
 $manifest['standalone'] = true;
+
+$manifest['github_update'] = 'ThemeFuse/Unyson-Portfolio-Extension';
