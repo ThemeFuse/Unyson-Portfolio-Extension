249643d70ed1ada07a1bcef45a0de3682fc2eba6
moldcraft <moldcraft@email.com>
2015-02-25 12:39:09 +0200

    Version 1.0.1

--- manifest.php
+++ manifest.php
@@ -6,7 +6,7 @@ $manifest = array();
 
 $manifest['name']        = __( 'Portfolio', 'fw' );
 $manifest['description'] = __( "This extension will add a fully fledged portfolio module that will let you display your projects using the built in portfolio pages.", 'fw' );
-$manifest['version'] = '1.0.0';
+$manifest['version'] = '1.0.1';
 $manifest['display'] = true;
 $manifest['standalone'] = true;
 
