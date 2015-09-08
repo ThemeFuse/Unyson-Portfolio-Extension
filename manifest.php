5da6cd7fc072c2043d7c4ffa95a93564682540e9
moldcraft <moldcraft@email.com>
2015-09-08 22:14:57 +0300

    Version 1.0.4

--- manifest.php
+++ manifest.php
@@ -10,7 +10,7 @@ $manifest['description'] = __(
 	.' using the built in portfolio pages.',
 	'fw'
 );
-$manifest['version'] = '1.0.3';
+$manifest['version'] = '1.0.4';
 $manifest['display'] = true;
 $manifest['standalone'] = true;
 
